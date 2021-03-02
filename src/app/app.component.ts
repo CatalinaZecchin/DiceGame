d5d8b8b20da9b862f1b34248318fdb3d8c98ea03
catalinazecchin <catalinazecchin@gmail.com>
2021-03-02 18:17:30 -0300

    Add functionality to the button. Now it throws two random numbers from 1 to 6 and prints them in the console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dados';
+  dadoIzq = "../assets/img/dice1.png";
+  dadoDer = "../assets/img/dice2.png";
+  numero1: number;
+  numero2: number;
+
+  tirarDados(): void {
+    this.numero1 = Math.round(Math.random() * 5 + 1);
+    this.numero2 = Math.round(Math.random() * 5 + 1);
+    console.log(this.numero1);
+    console.log(this.numero2);
+
+  }
 }
