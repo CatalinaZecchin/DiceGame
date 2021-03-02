452fdc8ac151ff9afc1796f2dedefa4c539d79a1
catalinazecchin <catalinazecchin@gmail.com>
2021-03-02 18:35:26 -0300

    Final commit, add ngIf directive to check when game is won, add You won button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   dadoIzq = "../assets/img/dice1.png";
   dadoDer = "../assets/img/dice2.png";
-  numero1: number;
-  numero2: number;
+  numero1: number = 1;
+  numero2: number = 5;
 
   tirarDados(): void {
     this.numero1 = Math.round(Math.random() * 5 + 1);
     this.numero2 = Math.round(Math.random() * 5 + 1);
-    console.log(this.numero1);
-    console.log(this.numero2);
+    this.dadoIzq = `../assets/img/dice${this.numero1}.png`;
+    this.dadoDer = `../assets/img/dice${this.numero2}.png`;
 
   }
 }
